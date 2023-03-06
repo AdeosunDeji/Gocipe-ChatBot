2690a48abf35acfcdc64e36a21bde7f3ca4aa3b1
AdesounDeji <HP@Ghostman.(none)>
2023-03-06 13:42:39 +0100

    first commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", async (req, res) => {
+  res.json({ status: true, message: "Our node.js app works" });
+});
+
+app.listen(PORT, () => console.log(`App listening at port ${PORT}`));
